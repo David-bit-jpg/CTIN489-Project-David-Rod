9ee7d09bdbeb8bcd9be2afa3b6d4f3bcb54f83b1
David-bit-jpg <67268612+David-bit-jpg@users.noreply.github.com>
2024-03-11 18:30:54 -0700

    1

--- Assets/Scripts/HorrorFace.cs
+++ Assets/Scripts/HorrorFace.cs
@@ -5,8 +5,11 @@ public class HorrorFace : MonoBehaviour
 {
     public float lifetime = 10f;
 
+    private PlayerMovement playerMovement;
+
     void Start()
     {
+        playerMovement =  FindObjectOfType<PlayerMovement>();
         StartCoroutine(DestroyAfterTime(lifetime));
     }
 
@@ -14,5 +17,15 @@ public class HorrorFace : MonoBehaviour
     {
         yield return new WaitForSeconds(time);
         Destroy(gameObject);
+    }
+    void Update()
+    {
+        if(playerMovement != null)
+        {
+            Vector3 direction = playerMovement.transform.position - transform.position;
+            direction.y = 0;
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = lookRotation;
+        }
     }
 }
