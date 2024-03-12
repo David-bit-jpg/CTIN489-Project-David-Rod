2a79aa2ffcd7c0440e9da53e878887258067a7f8
David-bit-jpg <67268612+David-bit-jpg@users.noreply.github.com>
2024-03-12 08:18:14 -0700

    update

--- Assets/Scripts/HorrorFace.cs
+++ Assets/Scripts/HorrorFace.cs
@@ -3,8 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 public class HorrorFace : MonoBehaviour
 {
-    public float lifetime = 10f;
-
+    public float lifetime;
+    private bool shouldRotate = true;
     private PlayerMovement playerMovement;
 
     void Start()
@@ -20,12 +20,16 @@ public class HorrorFace : MonoBehaviour
     }
     void Update()
     {
-        if(playerMovement != null)
+        if(playerMovement != null && shouldRotate)
         {
             Vector3 direction = playerMovement.transform.position - transform.position;
             direction.y = 0;
             Quaternion lookRotation = Quaternion.LookRotation(direction);
             transform.rotation = lookRotation;
         }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        shouldRotate = false;
     }
 }
