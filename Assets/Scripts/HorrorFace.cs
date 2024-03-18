a2bd20db2959589a67c118239f4f256f3f879a13
David-bit-jpg <67268612+David-bit-jpg@users.noreply.github.com>
2024-03-17 19:18:11 -0700

    Update HorrorFace.cs

--- Assets/Scripts/HorrorFace.cs
+++ Assets/Scripts/HorrorFace.cs
@@ -6,10 +6,11 @@ public class HorrorFace : MonoBehaviour
     public float lifetime;
     private bool shouldRotate = true;
     private PlayerMovement playerMovement;
+    private float lookTime = 0;
 
     void Start()
     {
-        playerMovement =  FindObjectOfType<PlayerMovement>();
+        playerMovement = FindObjectOfType<PlayerMovement>();
         StartCoroutine(DestroyAfterTime(lifetime));
     }
 
@@ -18,14 +19,37 @@ public class HorrorFace : MonoBehaviour
         yield return new WaitForSeconds(time);
         Destroy(gameObject);
     }
+
     void Update()
     {
-        if(playerMovement != null && shouldRotate)
+        if (playerMovement != null && shouldRotate)
         {
             Vector3 direction = playerMovement.transform.position - transform.position;
             direction.y = 0;
             Quaternion lookRotation = Quaternion.LookRotation(direction);
             transform.rotation = lookRotation;
+
+            if (IsPlayerLookingAtPrefab())
+            {
+                Debug.Log("LOOK");
+                lookTime += Time.deltaTime;
+                if (lookTime > 10)
+                {
+                    playerMovement.killed = true;
+                }
+            }
+            else
+            {
+                lookTime = 0;
+            }
         }
+    }
+
+    bool IsPlayerLookingAtPrefab()
+    {
+        Vector3 playerDirection = playerMovement.transform.forward;
+        Vector3 toPrefab = (transform.position - playerMovement.transform.position).normalized;
+        float angle = Vector3.Angle(playerDirection, toPrefab);
+        return angle < 30.0f;
     }
 }
