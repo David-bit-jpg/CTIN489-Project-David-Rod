169b518df0ba260497d1cd0b72ff4d4bf35e7d6a
Xiaolong Dong <dongxiao@usc.edu>
2024-04-07 01:16:17 -0700

    portal finished

---
+++ Assets/Scripts/Portal/Portal.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    public Portal OtherPortal;
+
+    private List<PortalableObject> portalObjects = new List<PortalableObject>();
+
+    // Components.
+    public Renderer Renderer { get; private set; }
+    private new BoxCollider collider;
+
+    private void Awake()
+    {
+        collider = GetComponent<BoxCollider>();
+        Renderer = GetComponent<Renderer>();
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+
+        for (int i = 0; i < portalObjects.Count; ++i)
+        {
+            Vector3 objPos = transform.InverseTransformPoint(portalObjects[i].transform.position);
+
+            if (objPos.z > 0.0f)
+            {
+                portalObjects[i].Warp();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var obj = other.GetComponent<PortalableObject>();
+        if (obj != null)
+        {
+            portalObjects.Add(obj);
+            obj.SetIsInPortal(this, OtherPortal);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var obj = other.GetComponent<PortalableObject>();
+
+        if (portalObjects.Contains(obj))
+        {
+            portalObjects.Remove(obj);
+            obj.ExitPortal();
+        }
+    }
+
+
+
+
+}
