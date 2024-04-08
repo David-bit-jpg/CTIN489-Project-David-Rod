34fc7d63133194b72d8af578c95b7e5364bbf953
Xiaolong Dong <dongxiao@usc.edu>
2024-04-08 03:23:38 -0700

    added cut scene

--- Assets/Scripts/Room1CutSceneManager.cs
+++ Assets/Scripts/Room1CutSceneManager.cs
@@ -1,9 +1,12 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Room1CutSceneManager : MonoBehaviour
 {
+    [SerializeField] Animator deadBodyAnimator;
+    [SerializeField] DoorController door;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,18 @@ public class Room1CutSceneManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (door.isOpened && !deadBodyAnimator.gameObject.activeSelf)
+        {
+            deadBodyAnimator.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<ConstantForce>())
+        {
+            deadBodyAnimator.SetBool("Hit", true);
+        }
+
     }
 }
